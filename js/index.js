df8a9dcfebc7bcabe62ef8ea3dcd4514176c1541
Menanya <emmmiemenz@gmail.com>
2022-05-19 20:34:39 +0100

    add summit picture effect

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
 const menuBtn = document.querySelector('.menu-bar');
 
 function showMenu() {
-  // const links = document.querySelector('.links-2');
-  // links.style.display = 'block'
-  // const displayMenu = document.querySelector('.links-2');
-  // displayMenu.style.left = '0%';
+  const logo = document.querySelector('.mobile .logo');
   const links = document.querySelector('.links-2');
   const toggle = document.querySelector('.open');
 
@@ -12,10 +9,12 @@ function showMenu() {
     links.style.left = '0';
     toggle.classList.add('fa-times');
     toggle.classList.remove('fa-bars');
+    logo.style.display = "none"
   } else {
     links.style.left = '-100%';
     toggle.classList.add('fa-bars');
     toggle.classList.remove('fa-times');
+    logo.style.display = "block"
   }
 }
 menuBtn.addEventListener('click', showMenu);
